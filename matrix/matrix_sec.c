d1053603d4f4fef61e91abeea2d42861eb27bdc6
sebasttianvelez <sebasttianvelez@gmail.com>
2020-02-14 21:50:57 -0500

    first commit

---
+++ matrix/matrix_sec.c
@@ -0,0 +1,10 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//medir tiempo real de cpu
+
+//medir el tiempo de cpu en ciclos de cpu
+
+//Implementar secuencial, e implementar con hilos
